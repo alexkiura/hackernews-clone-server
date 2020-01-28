be620908049c118e0125f96391bbe77c6595bab4
Alex Kiura <kiuraalex@gmail.com>
2020-01-28 22:54:11 +0300

    Add User Authentication schema

---
+++ hackernews/users/schema.py
@@ -0,0 +1,34 @@
+from django.contrib.auth import get_user_model
+
+import graphene
+from graphene_django import DjangoObjectType
+
+USER_MODEL = get_user_model()
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = USER_MODEL
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+  
+    def mutate(self, info, username, password, email):
+        user = USER_MODEL(
+            username=username,
+            email=email
+        )
+        user.set_password(password)
+        user.save()
+
+        return CreateUser(user=user)
+
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
