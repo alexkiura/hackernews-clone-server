3feef2ae531728f4a5da6906d67758df69fea7ff
Alex Kiura <kiuraalex@gmail.com>
2020-01-28 16:31:46 +0300

    Setup GraphQL
    Specify schema
    
    Remove newline

--- hackernews/hackernews/settings.py
+++ hackernews/hackernews/settings.py
@@ -30,14 +30,19 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+PREREQ_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
 ]
+
+PROJECT_APPS = []
+
+INSTALLED_APPS = [*PREREQ_APPS, *PROJECT_APPS]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -118,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE = {
+    'SCHEMA': 'hackernews.schema.schema',
+}
