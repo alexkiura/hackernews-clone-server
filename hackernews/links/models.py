859329f2aef1708c74e3a00cd99c980fdd05fd44
Alex Kiura <kiuraalex@gmail.com>
2020-01-30 13:02:44 +0300

    Add vote model

--- hackernews/links/models.py
+++ hackernews/links/models.py
@@ -8,3 +8,24 @@ class Link(models.Model):
     description = models.TextField(blank=True)
     posted_by = models.ForeignKey(
         settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+
+
+class Vote(models.Model):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    link = models.ForeignKey(
+        'links.Link',
+        related_name='votes',
+        on_delete=models.CASCADE
+    )
+
+
+    class Meta:
+        verbose_name = "Vote"
+        verbose_name_plural = "Votes"
+
+    def __str__(self):
+        return self.name
