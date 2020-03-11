1d30061a58d3ab3937f09a9103a7fa6edf3fc9b0
Alex Kiura <kiuraalex@gmail.com>
2020-03-11 14:17:00 +0300

    Add album

--- hackernews/links/models.py
+++ hackernews/links/models.py
@@ -1,7 +1,45 @@
+from datetime import datetime
+from typing import Union
+
 from django.db import models
 from django.conf import settings
 
 
+class KinveyDateField(models.DateTimeField):
+    """Field to map Django model fields to text and ISO8601 encoded dates"""
+
+    description = "Kinvey date field"
+
+    def __init__(self, *args, **kwargs):
+        kwargs["max_length"] = 32
+        super().__init__(*args, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        del kwargs["max_length"]
+        return name, path, args, kwargs
+
+    def get_prep_value(self, value):
+        return value.isoformat()
+
+    def db_type(self, connection) -> str:
+        return f"char({self.max_length})"
+
+    def value_to_string(self, obj):
+        value = self.value_from_object(obj)
+        return self.get_prep_value(value)
+
+    def process_to_datetime(self, value) -> Union[None, datetime]:
+        return datetime.fromisoformat(value)
+
+    def to_python(self, value) -> Union[None, datetime]:
+        if isinstance(value, datetime):
+            return value
+
+        if value is None:
+            return value
+
+        return self.process_to_datetime(value)
 # Create your models here.
 class Link(models.Model):
     url = models.URLField()
@@ -28,3 +66,21 @@ class Vote(models.Model):
 
     def __str__(self):
         return 'Vote'
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=100)
+    createdAt = models.DateField(auto_now_add=True)
+    updatedAt = models.DateField(auto_now=True)
+
+
+class Song(models.Model):
+    name = models.CharField(max_length=100)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+
+class Video(models.Model):
+    name = models.CharField(max_length=100)
+    createdAt = KinveyDateField(auto_now_add=True)
+    updatedAt = KinveyDateField(auto_now=True)
